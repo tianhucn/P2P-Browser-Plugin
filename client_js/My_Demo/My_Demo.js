1a8144b3585aac798b8e4acfa767fa68141cfc42
Liu-Sitong <liusit@foxmail.com>
2018-06-01 14:35:44 +0800

    My Demo is modify to be able to P2P between different device (theoretically)

--- client_js/My_Demo/My_Demo.js
+++ client_js/My_Demo/My_Demo.js
@@ -1,6 +1,9 @@
+
 var video;
-var hlsjsConfig;
-var p2pConfig;
+// var hlsjsConfig;
+// var p2pConfig;
+// var hls;
+// var p2pPlugin;
 
 var network, nodes, edges;
 var nodeIds;
@@ -11,14 +14,16 @@ var cpm = {
     table: document.querySelectorAll('#table-body tbody')[0]
 };
 var addToTable = function (sn, url, level, downloaded, source) {
-    // cpm.table.innerHTML +=
-    //     `<tr>
-    //                 <td>${sn}</td>
-    //                 <td>${url}</td>
-    //                 <td>${level}</td>
-    //                 <td>${downloaded}</td>
-    //                 <td>${source}</td>
-    //             </tr>`;
+    console.log(cpm.table);
+    console.log("sagasdg");
+    cpm.table.innerHTML +=
+        `<tr>
+                    <td>${sn}</td>
+                    <td>${url}</td>
+                    <td>${level}</td>
+                    <td>${downloaded}</td>
+                    <td>${source}</td>
+                </tr>`;
     if (!array_contain(nodeIds, sn)) {
         nodes.add({id: sn, label: String(sn)});
         nodeIds.push(sn);
@@ -56,23 +61,7 @@ console.log('p2p plugin version: ' + Hls.pluginVersion);
 
 
 if (Hls.isSupported()) {
-    video = document.getElementById('video');
 
-    hlsjsConfig = {
-//        debug: true,
-//      pLoader : pLoader,
-        maxBufferSize: 0,
-    };
-
-    p2pConfig = {
-        debug: true,
-//            websocketAddr: 'ws://120.78.168.126:3389',
-        websocketAddr: 'ws://localhost:3389',
-        reportInterval: 36000,
-        p2pEnabled: true
-    };
-
-    hlsjsConfig.p2pConfig = p2pConfig;
     playVideo();
 
 //        hls.disableP2P();
@@ -84,12 +73,37 @@ if (Hls.isSupported()) {
 
 
 function playVideo() {
+    video = document.getElementById('video');
+
+    var hlsjsConfig = {
+//        debug: true,
+//      pLoader : pLoader,
+        liveSyncDuration: 60,
+        fragLoadingTimeOut: 4000,
+    };
+
+    var p2pConfig = {
+        key: 'free',
+        wsSchedulerAddr: `ws://10.21.108.21:8080/ws`,
+        wsSignalerAddr: 'ws://120.78.168.126:8081/ws',
+        reportInterval: 30,
+//            defaultUploadBW: 2561241,
+//            defaultUploadBW: 2561241 + 100,
+//            defaultUploadBW: 853290*3+1000,
+        defaultUploadBW: 2557670/3*5,
+//            transitionEnabled: true,
+        transitionEnabled: false,
+        transitionCheckInterval: 90
+    };
+    var hls = new Hls(hlsjsConfig);
+    var p2pPlugin = new HlsPeerify(hls, p2pConfig);
+
     nodeIds = new Array();
     nodeInformation = {};
     createTopo();
     document.getElementById('node-info').innerHTML = "";
-    // cpm.table.innerHTML = ""
-    var hls = new Hls(hlsjsConfig);
+    cpm.table.innerHTML = "";
+
 //        hls.loadSource('https://video-dev.github.io/streams/x36xhzz/x36xhzz.m3u8');
 //        hls.loadSource('https://video-dev.github.io/streams/test_001/stream.m3u8');
     var videoURL = document.getElementById("videoURL").value;
@@ -99,14 +113,11 @@ function playVideo() {
     hls.on(Hls.Events.MANIFEST_PARSED, function (event, data) {
         hls.loadLevel = 0;
         video.play();
-        cpm.downloadresult.classList.remove('sr-only');
     });
     hls.on(Hls.Events.FRAG_LOADED, (id, data) => {
-
         var frag = data.frag;
-//            console.warn(JSON.stringify(data.frag));
 //            console.warn(`sn ${frag.sn} relurl ${frag.relurl} level ${frag.level} downloaded ${frag.loaded} source ${frag.loadByXhr?'CDN':'P2P'}`);
-        var source = frag.loadByXhr ? 'CDN' : 'P2P';
+        var source = frag.loadByXhr?'CDN':'P2P';
         addToTable(frag.sn, frag.relurl, frag.level, frag.loaded, source);
     });
 
@@ -114,23 +125,14 @@ function playVideo() {
         var errorType = data.type;
         var errorDetails = data.details;
         var errorFatal = data.fatal;
-        console.warn('warn details:' + errorDetails);
-//      switch(data.details) {
-//        case hls.ErrorDetails.FRAG_LOAD_ERROR:
-//          // ....
-//          break;
-//        default:
-//          break;
-//      }
+        console.warn('error details:'+errorDetails);
     });
-    // var p2pPlugin = new HlsPeerify(hls, p2pConfig);
-    // p2pPlugin.enableP2P();
-    // p2pPlugin._init(videoURL);
+
 
 }
 
 function resetURL() {
-    document.getElementById("videoURL").value = '//bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8'
+    document.getElementById("videoURL").value = 'https://video-dev.github.io/streams/x36xhzz/x36xhzz.m3u8'
     playVideo();
 }
 
